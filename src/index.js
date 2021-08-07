80895b9059ec5d286d46d9db3e72ab472a09d471
Rhyine <rhyinestewart@gmail.com>
2021-08-07 17:16:16 -0500

    Add bootstrap to boiler plate

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import _ from 'lodash';
 import './style.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function component() {
     const element = document.createElement('div');
   
     // Lodash, currently included via a script, is required for this line to work
     // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.innerHTML = _.join(['Starter', 'Text'], ' ');
     element.classList.add('hello');
   
     return element;
